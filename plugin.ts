282144c00e911fb1e8923e534715d541874eb226
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2025-03-08 01:53:00 +0900

    improve: Add hint

--- plugin.ts
+++ plugin.ts
@@ -21,14 +21,16 @@ export default {
               importDeclarationAttr.betweenPreviousStatement.split(lineBreak)
                 .at(-1) ?? "";
             const quote = node.source.raw[0];
+            const typeCommentDirective = "// @ts-types=" + quote +
+              node.source.value + ".d.ts" + quote;
 
             return context.report({
               message: "This css module import needs @ts-type directive.",
+              hint: "Add `" + typeCommentDirective + "` on top of import",
               fix(fixer) {
                 return fixer.insertTextBefore(
                   node,
-                  "// @ts-types=" + quote + node.source.value +
-                    ".d.ts" + quote + lineBreak + indent,
+                  typeCommentDirective + lineBreak + indent,
                 );
               },
               range: node.range,
